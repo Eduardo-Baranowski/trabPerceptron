b39ecc3ba3144d334ae583fa9ac9069109058789
Eduardo-Baranowski <eduardobaranowski@gmail.com>
2020-05-12 13:46:11 -0300

    versao1

---
+++ perceptron.py
@@ -0,0 +1,140 @@
+'''
+	Implementação da rede neural Perceptron
+
+	w = w + N * (d(k) - y) * x(k)
+'''
+
+import random, copy
+
+class Perceptron:
+
+	def __init__(self, amostras, saidas, taxa_aprendizado=0.1, epocas=10000, limiar=-1):
+
+		self.amostras = amostras # todas as amostras
+		self.saidas = saidas # saídas respectivas de cada amostra
+		self.taxa_aprendizado = taxa_aprendizado # taxa de aprendizado (entre 0 e 1)
+		self.epocas = epocas # número de épocas
+		self.limiar = limiar # limiar
+		self.num_amostras = len(amostras) # quantidade de amostras
+		self.num_amostra = len(amostras[0]) # quantidade de elementos por amostra
+		self.pesos = [] # vetor de pesos
+
+
+	# função para treinar a rede
+	def treinar(self):
+
+		# adiciona -1 para cada uma das amostras
+		for amostra in self.amostras:
+			amostra.insert(0, -1)
+
+
+		# inicia o vetor de pesos com valores aleatórios
+		for i in range(self.num_amostra):
+			self.pesos.append(random.randint(0,1))
+
+		# insere o limiar no vetor de pesos
+		self.pesos.insert(0, self.limiar)
+
+		# inicia o contador de epocas
+		num_epocas = 0
+
+
+
+		while True:
+
+			erro = False # o erro inicialmente inexiste
+
+			# para todas as amostras de treinamento
+			for i in range(self.num_amostras):
+
+				u = 0
+
+				'''
+					realiza o somatório, o limite (self.amostra + 1)
+					é porque foi inserido o -1 para cada amostra
+				'''
+
+				for j in range(self.num_amostra + 1):
+
+					u += self.pesos[j] * self.amostras[i][j]
+
+
+				# obtém a saída da rede utilizando a função de ativação
+				y = self.sinal(u)
+
+				# verifica se a saída da rede é diferente da saída desejada
+				if y != self.saidas[i]:
+
+					# calcula o erro: subtração entre a saída desejada e a saída da rede
+					erro_aux = self.saidas[i][0] - y
+
+					# faz o ajuste dos pesos para cada elemento da amostra
+					for j in range(self.num_amostra + 1):
+						self.pesos[j] = self.pesos[j] + self.taxa_aprendizado * erro_aux * self.amostras[i][j]
+
+					erro = True # ainda existe erro
+
+				if y != self.saidas[i]:
+
+					# calcula o erro: subtração entre a saída desejada e a saída da rede
+					erro_aux = self.saidas[i][1] - y
+
+					# faz o ajuste dos pesos para cada elemento da amostra
+					for j in range(self.num_amostra + 1):
+						self.pesos[j] = self.pesos[j] + self.taxa_aprendizado * erro_aux * self.amostras[i][j]
+
+					erro = True # ainda existe erro
+
+
+			# incrementa o número de épocas
+			num_epocas += 1
+			#print('Em época número ', str(num_epocas), 'amostra é ', self.amostras)
+
+			# critério de parada é pelo número de épocas ou se não existir erro
+			if num_epocas > self.epocas or not erro:
+				break
+
+
+	# função utilizada para testar a rede
+	# recebe uma amostra a ser classificada e os nomes das classes
+	# utiliza a função sinal, se é -1 então é classe1, senão é classe2
+	def testar(self, amostra, classe1, classe2):
+		# insere o -1
+		amostra.insert(0, -1)
+
+		# utiliza o vetor de pesos que foi ajustado na fase de treinamento
+		u = 0
+		for i in range(self.num_amostra + 1):
+			u += self.pesos[i] * amostra[i]
+		y = self.sinal(u)
+		if y == -1:
+			print('A amostra ' + str(amostra) +' pertence a classe %s' % classe1 + ' com peso de ' + str(u))
+		else:
+			print('A amostra ' + str(amostra) +' pertence a classe %s' % classe2 + ' com peso de ' + str(u))
+
+	def sinal(self, u):
+		return 1 if u >= 0 else -1
+
+
+
+# amostras: um total de 4 amostras
+amostras = [[1, 0, 1, 0], [0, 1, 0, 1], [1, 0, 0, 1], [0, 0, 0, 1]]
+
+# saídas desejadas de cada amostra
+saidas = [0, 1], [1, 0], [1, 0], [0, 1]
+
+# conjunto de amostras de testes
+testes = copy.deepcopy(amostras)
+
+# cria uma rede Perceptron
+rede = Perceptron(amostras=amostras, saidas=saidas, taxa_aprendizado=0.1, epocas=10000)
+
+# treina a rede
+rede.treinar()
+
+# testando a rede
+print('Vetor com as amostras', amostras, '\n')
+print('Vetor de pesos', rede.pesos, '\n')
+for teste in testes:
+
+	rede.testar(teste, 'A', 'B')
